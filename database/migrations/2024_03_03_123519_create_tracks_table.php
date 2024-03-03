136356a2c05e24a3c9f4503892a41fe99604a563
Alexey <test@git.ru>
2024-03-03 18:50:27 +0300

    init

---
+++ database/migrations/2024_03_03_123519_create_tracks_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tracks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('singer_id');
+            $table->foreignId('type_id');
+            $table->string('name');
+            $table->double('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tracks');
+    }
+};
---
+++ database/migrations/2024_03_03_123553_create_payments_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('track_id');
+            $table->double('price');
+            $table->enum('status', ['done', 'fail', 'wait']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
